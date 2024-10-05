39970a8e3181edc024d87125275ccbf420b83319
Gloeidraad <155532921+Gloeidraad@users.noreply.github.com>
2024-10-05 23:46:41 +0200

    Delete Software/ESP32-Music-Player directory

--- Software/ESP32-Music-Player/src/ChipInfo.h
+++
@@ -1,6 +0,0 @@
-#ifndef _CHIPINFO_H_
-#define _CHIPINFO_H_
-
-void PrintChipInfo(int flags = 0xFF);
-
-#endif // _CHIPINFO_H_
--- Software/ESP32-Music-Player/src/I2C_Scanner.h
+++
@@ -1,9 +0,0 @@
-#ifndef _I2C_SCANNER_H_
-#define _I2C_SCANNER_H_
-
-#include <Wire.h>
-
-void Scan_I2C(TwoWire &wire = Wire, uint8_t bus_id = 0);
-bool Scan_I2C_Device(TwoWire &wire, uint8_t address);
-
-#endif
